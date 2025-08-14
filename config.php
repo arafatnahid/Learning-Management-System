65f0093901dad36350608529b37d660350b3f327
arafatnahid <arafatnahid20@gmail.com>
2025-08-14 23:59:42 +0600

    Initial commit

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mariadb';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'araflgpe_mood572';
+$CFG->dbuser    = 'araflgpe_mood572';
+$CFG->dbpass    = '.fpS1B5@Cx[UT()8';
+$CFG->prefix    = 'mdlmb_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_general_ci',
+);
+
+$CFG->wwwroot   = 'https://arafatnahid.com/edtech';
+$CFG->dataroot  = '/home/araflgpe/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
